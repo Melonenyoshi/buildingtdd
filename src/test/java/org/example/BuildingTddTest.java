fc1d20424e47500d563dd75132cf8a0006ad1206
Melonenyoshi <lodi.federsel@gmail.com>
2022-01-14 11:19:21 +0100

    first commit

---
+++ src/test/java/org/example/BuildingTddTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class BuildingTddTest
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+
+    }
+}
